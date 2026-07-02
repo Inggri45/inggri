b93a594d463f38920eeba097de3779d40914b42a
Inggri Eka Pratiwi <inggriekapratiwi@gmail.com>
2026-07-02 23:23:49 +0700

    gallery

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { AfterViewInit , Component, Renderer2 } from '@angular/core';
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
+import { Sidebar } from '../sidebar/sidebar';
+import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+declare const $: any;
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [Header, Sidebar, Footer, RouterModule],
   templateUrl: './gallery.html',
-  styleUrl: './gallery.css',
+  styleUrls: ['./gallery.css'],
 })
 export class Gallery {}
